53c0768f76bf7089381bb4bbcee79528ae718ff7
nstephens13 <100706601+nstephens13@users.noreply.github.com>
2025-06-26 07:44:24 +0200

    feat: add Book class and define other endspoints

--- src/main/java/org/agoncal/quarkus/starting/BookResource.java
+++ src/main/java/org/agoncal/quarkus/starting/BookResource.java
@@ -2,15 +2,37 @@ package org.agoncal.quarkus.starting;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import java.util.List;
+import java.util.Optional;
+
 @Path("/api/books")
+@Produces(MediaType.APPLICATION_JSON)
 public class BookResource {
 
     @GET
+    public List<Book> getAllBooks() {
+        return List.of(
+                new Book(1, "Book 1", "Author 1", 2020, "IT"),
+                new Book(2, "Book 2", "Author 2", 2020, "IT"),
+                new Book(3, "Book 3", "Author 3", 2020, "IT"),
+                new Book(4, "Book 4", "Author 4", 2020, "IT")
+        );
+    }
+
+    @GET
+    @Path("/count")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    public int countAllBooks() {
+        return getAllBooks().size();
+    }
+
+    @GET
+    @Path("{id}")
+    public Optional<Book> getBook(@PathParam("id") int id) {
+        return getAllBooks().stream().filter(book -> book.id == id).findFirst();
     }
 }
